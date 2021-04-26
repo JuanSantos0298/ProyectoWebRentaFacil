5569e70925b0afd4e55129fa9f49247f632cc802
Alex Uriel Acoltzi <alex.fantastico22@gmail.com>
2021-04-25 21:30:43 -0500

    Arreglos para ver ganancias

--- ProyectoWebRentaFacil/src/app/mis-ganancias/mis-ganancias.component.ts
+++ ProyectoWebRentaFacil/src/app/mis-ganancias/mis-ganancias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 import { ServicioService } from '../Servicios/servicio.service';
 
 @Component({
@@ -12,6 +13,9 @@ export class MisGananciasComponent implements OnInit {
 
   correo='';
   rentas: any[]=[];
+  mapRentas = new Map();
+  arrayObjetos: any;
+  total:Observable<number>;
 
   constructor(private aRout: ActivatedRoute,private _ganancias:ServicioService,private toas: ToastrService) {
     this.correo = this.aRout.snapshot.paramMap.get('correo');
@@ -21,6 +25,7 @@ export class MisGananciasComponent implements OnInit {
     this.tabla();
   }
 
+  //funcion para obtener los datos de las casas reservadas
   tabla()
   {
     this._ganancias.visualizarGanancias(this.correo).subscribe((data: any)=>{
@@ -31,8 +36,34 @@ export class MisGananciasComponent implements OnInit {
           ...elemnt.payload.doc.data(),
         })
       });
-      console.log(this.rentas);
+      this.map(this.rentas);
+      //console.log(this.rentas);
     })
+  }
+
+  //funcion para mapear los datos que se mostraran en ganancias
+  map(value: any){
+    value.forEach(element => {
+      if (!this.mapRentas.has(element.Ubicacion)) {
+        this.mapRentas.set(element.Ubicacion, {Ubicacion: element.Ubicacion, fecha: ["entrada: " + element.Entrada + " salida:" + element.Salida], estado: element.Estado, municipio: element.Municipio, total: element.Pago, rentas: ["$" + element.Pago]})
+      } else {
+        const obj = this.mapRentas.get(element.Ubicacion);
+        obj.total = obj.total + element.Pago;
+        obj.rentas.push("\n $" + element.Pago);
+        obj.fecha.push("\n" + "entrada: " + element.Entrada + " salida: " + element.Salida)
+        this.mapRentas.set(element.Ubicacion,obj);
+      }
+    });
+
+    this.arrayObjetos = Array.from(this.mapRentas.values());
+    console.log(this.arrayObjetos);
+    this.Totales(this.arrayObjetos);
+  }
+
+  //funcion para obtener las ganancias totales por casas
+  Totales(value: any){
+  this.total = value.reduce((prev, curr) => prev.total + curr.total);
+  console.log(this.total);
   }
 
 }
